be2eaacf2533d7eea6302df2b4ad67e70d3995e5
Mohammed Jewel <jewelnabin@gmail.com>
2022-04-10 14:28:16 +0600

    addeed onAuthState change to persist the auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}>Home</Route>
-        <Route path='/home'>Home</Route>
+        <Route path='/products' element={<Products></Products>}>Products</Route>
+        <Route path='/orders' element={<Orders></Orders>}>Orders</Route>
+        <Route path='/register' element={<Register></Register>}>Home</Route>
         <Route path='/login' element={<Login></Login>}>Login</Route>
       </Routes>
     </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please Log In</h3>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Signin</button>
+            </div>
+            <form>
+                <input type="email" name="" id="opiurew43879" placeholder='Your Eamil' />
+                <br />
+                <input type="password" name="" id="440iesr094930j" placeholder='password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
